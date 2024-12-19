e383cf94ebcbd5d79f68cbaa8b527dc691cd369e
imakhija <46541595+imakhija@users.noreply.github.com>
2024-12-19 17:29:53 -0500

    revert

--- mode.js
+++ mode.js
@@ -39,3 +39,23 @@ function change() {
   }
   document.getElementById("mode").style.fontWeight = "bold";
 }
+
+window.onload = function() {
+    fetch('navbar.html')
+    .then(response => response.text())
+    .then(navbarData => {
+        document.getElementById('navbar').innerHTML = navbarData;
+        return fetch('footer.html');
+    })
+    .then(response => response.text())
+    .then(footerData => {
+        document.getElementById('footer').innerHTML = footerData;
+        window.scrollTo(0, 0);
+        document.body.style.visibility = 'visible';
+        document.body.style.opacity = '1';
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+    })
+    .catch(err => {
+        console.log('Error loading navbar or footer:', err);
+    });
+};
