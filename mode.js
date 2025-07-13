4b7d8f4f000de4d1790e96db57e0bd27b2b1243f
imakhija <46541595+imakhija@users.noreply.github.com>
2025-07-13 01:14:44 -0400

    redesigned navbar

--- mode.js
+++ mode.js
@@ -1,43 +1,35 @@
 function setMode() {
   if(localStorage.getItem("theme") == "dark") {
       document.documentElement.setAttribute('data-theme', "dark");
-      document.getElementById("mode").innerHTML = "light mode";
       localStorage.setItem("theme", "dark");
   }
   else {
       document.documentElement.setAttribute('data-theme', "light");
-      document.getElementById("mode").innerHTML = "dark mode";
       localStorage.setItem("theme", "light");
   }
-  document.getElementById("mode").style.fontWeight = "bold";
 }
 
 function change() {
   if(localStorage.getItem("theme") === null) {
       if(document.documentElement.getAttribute("data-theme") === "light") {
           document.documentElement.setAttribute('data-theme', "dark");
-          document.getElementById("mode").innerHTML = "light mode";
           localStorage.setItem("theme", "dark");
       }
       else {
           document.documentElement.setAttribute('data-theme', "light");
-          document.getElementById("mode").innerHTML = "dark mode";
           localStorage.setItem("theme", "light");
       }
   }
   else {
       if(localStorage.getItem("theme") == "dark") {
           document.documentElement.setAttribute('data-theme', "light");
-          document.getElementById("mode").innerHTML = "dark mode";
           localStorage.setItem("theme", "light");
       }
       else {
           document.documentElement.setAttribute('data-theme', "dark");
-          document.getElementById("mode").innerHTML = "light mode";
           localStorage.setItem("theme", "dark");
       }
   }
-  document.getElementById("mode").style.fontWeight = "bold";
 }
 
 window.onload = function() {
