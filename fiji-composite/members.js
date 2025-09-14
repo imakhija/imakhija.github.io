3c75e9aaf8c60a7546258b58cef90bb6413d82f7
imakhija <46541595+imakhija@users.noreply.github.com>
2025-09-14 17:36:51 -0400

    hosting fraternity composite

---
+++ fiji-composite/members.js
@@ -0,0 +1,14 @@
+const members = [
+    {
+      name: "John Doe",
+      position: "President",
+      classYear: 2026,
+      photo: "photos/john_doe.jpg"
+    },
+    {
+      name: "Jane Smith",
+      position: "Treasurer",
+      classYear: 2025,
+      photo: "photos/jane_smith.jpg"
+    },
+  ];  
